22c8a063bb331891eb04e80edc0ebdfcd29fb993
issaojr <issaojr@gmail.com>
2019-02-20 00:50:35 -0300

    Created Contact and Kind. Other minor updates.

---
+++ app/models/contact.rb
@@ -0,0 +1,26 @@
+class Contact < ApplicationRecord
+    belongs_to :kind
+
+    # def birthdate_br
+    #     I18n.l(self.birthdate) unless self.birthdate.blank?
+    # end
+
+    def to_br
+        { name: self.name, birthdate: ( I18n.l(self.birthdate) unless self.birthdate.blank?)}
+    end
+    # def author
+    #     "Issao Jr."
+    # end
+
+    # def kind_description
+    #     self.kind.description
+    # end
+
+    # def as_json(options={})
+    #     super(
+    #         root: true,
+    #         methods: [:kind_description, :author],
+    #         include: { kind: { only: :description }}
+    #         )
+    # end
+end
